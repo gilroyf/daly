c99387e9180ed188eb01571bd4a2aa43e2a0f362
fernandes.gilroy@gmail.com <fernandes.gilroy@gmail.com>
2018-10-10 12:17:16 +0000

    added dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,55 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader"
+module.exports = (env, args) => {
+  const envIsProduction = env === "production";
+  console.log("isProduction = ", envIsProduction);
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
           }
-        ]
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: [ 
-          'style-loader', 
-          'css-loader', 
-          'sass-loader'
-        ]
-      },
-      { 
-        test: /\.(png|jpg|gif)$/,
-        include: path.join(__dirname, 'dist/images'),
-        loader: 'url-loader' 
-     } 
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ],
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    historyApiFallback: true
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader"
+            }
+          ]
+        },
+        {
+          test: /\.(css|scss)$/,
+          use: [ 
+            'style-loader', 
+            'css-loader', 
+            'sass-loader'
+          ]
+        },
+        { 
+          test: /\.(png|jpg|gif)$/,
+          include: path.join(__dirname, 'dist/images'),
+          loader: 'url-loader' 
+      } 
+      ]
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+      })
+    ],
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devtool: envIsProduction ? "source-maps" : "cheap-module-eval-source-map",
+    devServer: {
+      historyApiFallback: true
+    }
   }
 };
